a58bf6c7c463b18b9da52c38767ffc293ec15971
mehadishakil <mehadishakil469@gmail.com>
2024-02-17 23:44:08 +0600

    init

---
+++ app/src/main/java/com/example/amplify/components/AppComponents.kt
@@ -0,0 +1,155 @@
+package com.example.loginsignup_jetpackcompose.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.amplify.R
+import com.example.amplify.ui.theme.BgColor
+import com.example.amplify.ui.theme.PrimaryColor
+import com.example.amplify.ui.theme.componenetShapes
+
+
+@Composable
+fun NormalTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp),
+        style = TextStyle(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Normal,
+            fontStyle = FontStyle.Normal
+        ),
+        color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
+    )
+}
+
+
+@Composable
+fun HeadingTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal
+        ),
+        color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
+    )
+}
+
+
+@Composable
+fun MyTextField(labelValue: String, painterResource: Painter) {
+    var textValue by remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(componenetShapes.small),
+        value = textValue,
+        onValueChange = { textValue = it },
+        label = { Text(labelValue) },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = BgColor,
+            focusedBorderColor = PrimaryColor,
+            focusedLabelColor = PrimaryColor,
+            cursorColor = PrimaryColor
+        ),
+        keyboardOptions = KeyboardOptions.Default,
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription = "")
+        }
+    )
+}
+
+
+
+@Composable
+fun PasswordTextField(labelValue: String, painterResource: Painter) {
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    val passwordVisible = remember {
+        mutableStateOf(false)
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(componenetShapes.small),
+        value = password,
+        onValueChange = { password = it },
+        label = { Text(labelValue) },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = BgColor,
+            focusedBorderColor = PrimaryColor,
+            focusedLabelColor = PrimaryColor,
+            cursorColor = PrimaryColor
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription = "")
+        },
+        trailingIcon = {
+            val iconImage = if(passwordVisible.value){
+                Icons.Filled.Visibility
+            } else {
+                Icons.Filled.VisibilityOff
+            }
+
+            var description = if(passwordVisible.value){
+                "Hide password"
+            }else {
+                "Show password"
+            }
+
+            IconButton(onClick = {passwordVisible.value = !passwordVisible.value}){
+                Icon(imageVector = iconImage, contentDescription = "")
+            }
+
+
+        },
+
+        visualTransformation = if(passwordVisible.value) VisualTransformation.None else
+            PasswordVisualTransformation()
+
+    )
+}
+
+
+
---
+++ app/src/main/java/com/example/amplify/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.amplify.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Blue = Color(0xFF0066FF)
+val Black = Color(0xFF1E1F22)
+
+
+val PrimaryColor = Color(0xFF80BCBD)
+val SecondaryColor = Color(0xFFAAD9BB)
+val TextColorColor = Color(0xFF181819)
+val AccentColor = Color(0xFFD5F0C1)
+val GrayColor = Color(0xFF7B6F72)
+val WhiteColor = Color(0xFFFFFFFF)
+val BgColor = Color(0xFFF7F8F8)
