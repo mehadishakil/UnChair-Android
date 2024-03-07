f33b299c21e30b7031acd6e8fd839679d62bf5f5
mehadishakil <mehadishakil469@gmail.com>
2024-03-07 20:42:13 +0600

    CircularProgressIndicator component added

--- app/src/main/java/com/example/amplify/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amplify/screens/HomeScreen.kt
@@ -1,8 +1,12 @@
 package com.example.loginsignup_jetpackcompose.screens
 
+import android.graphics.Paint
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,26 +35,66 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.amplify.R
 import androidx.compose.runtime.*
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import com.example.amplify.ui.theme.darkGray
+import com.example.amplify.ui.theme.gray
+import com.example.amplify.ui.theme.orange
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.roundToInt
+import kotlin.math.sin
 
 
 @Composable
 fun HomeScreen() {
-    Column(
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(Color.White)
+//    ) {
+//        Spacer(modifier = Modifier.height(20.dp))
+//        SedentaryTime()
+//
+//    }
+
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
-    ) {
-        Spacer(modifier = Modifier.height(20.dp))
-        SedentaryTime()
-
+            .background(darkGray),
+        contentAlignment = Alignment.Center
+    ){
+        CustomCircularProgressIndicator(
+            modifier = Modifier
+                .size(250.dp)
+                .background(darkGray),
+            initalValue = 50,
+            primaryColor = orange,
+            secondaryColor = gray,
+            circleRadius = 230f,
+            onPositionChange = {position ->
+                 // do something with this position value
+            }
+        )
     }
+
 }
 
 
@@ -86,17 +132,18 @@ fun SedentaryTime() {
                 verticalArrangement = Arrangement.SpaceEvenly,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "Sedentary Time",
+                Text(
+                    text = "Sedentary Time",
                     color = Color.Black,
                     fontFamily = FontFamily.SansSerif,
                     fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp)
+                    fontSize = 24.sp
+                )
                 DisplayTxtClock()
             }
         }
     }
 }
-
 
 
 //@Composable
@@ -113,9 +160,6 @@ fun SedentaryTime() {
 //        }
 //    )
 //}
-
-
-
 
 
 @Composable
@@ -180,11 +224,197 @@ fun getMillisFromTimeString(timeString: String): Long {
 }
 
 
+@Composable
+fun CustomCircularProgressIndicator(
+    modifier: Modifier = Modifier,
+    initalValue: Int,
+    primaryColor: Color,
+    secondaryColor: Color,
+    minValue: Int = 0,
+    maxValue: Int = 100,
+    circleRadius: Float,
+    onPositionChange: (Int) -> Unit
+) {
+    var circleCenter by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
+    var positionValue by remember {
+        mutableStateOf(initalValue)
+    }
+
+    var changeAngle by remember {
+        mutableStateOf(0f)
+    }
+
+    var dragStartedAngle by remember {
+        mutableStateOf(0f)
+    }
+
+    var oldPositionValue by remember {
+        mutableStateOf(initalValue)
+    }
+
+    Box(
+        modifier = modifier
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(true) {
+                    detectDragGestures(
+                        onDragStart = { offset ->
+                            dragStartedAngle = -atan2(
+                                x = circleCenter.y - offset.y,
+                                y = circleCenter.x - offset.x
+                            ) * (180 / Math.PI).toFloat()
+                            dragStartedAngle = (dragStartedAngle + 180f).mod(360f)
+                        },
+                        onDrag = { change, _ ->
+                            var touchAngle = -atan2(
+                                x = circleCenter.y - change.position.y,
+                                y = circleCenter.x - change.position.x
+                            ) * (180 / Math.PI).toFloat()
+
+                            touchAngle = (touchAngle + 180f).mod(360f)
+
+                            val currentAngle = oldPositionValue * 360f / (maxValue - minValue)
+                            changeAngle = touchAngle - currentAngle
+
+                            val lowerThreshold = currentAngle - (360f / (maxValue - minValue) * 5)
+                            val higherThreshold = currentAngle + (360f / (maxValue - minValue) * 5)
+
+                            if (dragStartedAngle in lowerThreshold..higherThreshold) {
+                                positionValue =
+                                    (oldPositionValue + (changeAngle / (360f / (maxValue - minValue))).roundToInt())
+                            }
+
+
+                        },
+                        onDragEnd = {
+                            oldPositionValue = positionValue
+                            onPositionChange(positionValue)
+                        }
+                    )
+                }
+        ) {
+            val width = size.width
+            val height = size.height
+            val circleThickness = width / 25f
+            circleCenter = Offset(x = width / 2f, y = height / 2f)
+
+            drawCircle(
+                brush = Brush.radialGradient(
+                    listOf(
+                        primaryColor.copy(0.45f),
+                        secondaryColor.copy(0.15f)
+                    )
+                ),
+                radius = circleRadius,
+                center = circleCenter
+            )
+
+
+            drawCircle(
+                style = Stroke(
+                    width = circleThickness,
+                ),
+                color = Color.Gray,
+                radius = circleRadius,
+                center = circleCenter
+            )
 
 
 
-@Preview
+            drawArc(
+                color = primaryColor,
+                startAngle = 90f,
+                sweepAngle = (360f / maxValue) * positionValue.toFloat(),
+                style = Stroke(
+                    width = circleThickness,
+                    cap = StrokeCap.Round
+                ),
+                useCenter = false,
+                size = Size(
+                    width = circleRadius * 2f,
+                    height = circleRadius * 2f
+                ),
+                topLeft = Offset(
+                    x = (width - circleRadius * 2f) / 2f,
+                    y = (height - circleRadius * 2f) / 2f
+                )
+            )
+
+            val outerRadius = circleRadius + circleThickness / 2f
+            val gap = 15f
+            for (i in 0..(maxValue - minValue)) {
+                val color = if (i < positionValue - minValue) primaryColor else primaryColor.copy(alpha = 0.3f)
+                val angleInDegree = i * 360f / (maxValue - minValue).toFloat()
+                val angleInRad = angleInDegree * Math.PI / 180f + Math.PI / 2f
+
+                val yGapAdjustment = cos(angleInDegree * Math.PI / 180f) * gap
+                val xGapAdjustment = -sin(angleInDegree * Math.PI / 180f) * gap
+
+
+                val start = Offset(
+                    x = (outerRadius * cos(angleInRad) + circleCenter.x + xGapAdjustment).toFloat(),
+                    y = (outerRadius * sin(angleInRad) + circleCenter.y + yGapAdjustment).toFloat()
+                )
+
+                val end = Offset(
+                    x = (outerRadius * cos(angleInRad) + circleCenter.x + xGapAdjustment).toFloat(),
+                    y = (outerRadius * sin(angleInRad) + circleThickness + circleCenter.y + yGapAdjustment).toFloat()
+                )
+
+                rotate(
+                    angleInDegree,
+                    pivot = start
+                ) {
+                    drawLine(
+                        color = color,
+                        start = start,
+                        end = end,
+                        strokeWidth = 1.dp.toPx()
+                    )
+                }
+            }
+
+            drawContext.canvas.nativeCanvas.apply {
+                drawIntoCanvas {
+                    drawText(
+                        "$positionValue %",
+                        circleCenter.x,
+                        circleCenter.y+45.dp.toPx()/3f,
+                        Paint().apply {
+                            textSize = 38.dp.toPx()
+                            textAlign = Paint.Align.CENTER
+                            color = Color.White.toArgb()
+                            isFakeBoldText = true
+                        }
+                    )
+                }
+            }
+
+
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    CustomCircularProgressIndicator(
+        modifier = Modifier
+            .size(250.dp)
+            .background(darkGray),
+        initalValue = 30,
+        primaryColor = orange,
+        secondaryColor = darkGray,
+        circleRadius = 230f,
+        onPositionChange = {
+
+        }
+    )
 }
--- app/src/main/java/com/example/amplify/ui/theme/Color.kt
+++ app/src/main/java/com/example/amplify/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Blue = Color(0xFF0066FF)
+//val Blue = Color(0xFF0066FF)
 val Black = Color(0xFF1E1F22)
 
 
@@ -21,3 +21,17 @@ val AccentColor = Color(0xFFD5F0C1)
 val GrayColor = Color(0xFF7B6F72)
 val WhiteColor = Color(0xFFFFFFFF)
 val BgColor = Color(0xFFF7F8F8)
+
+
+val purple = Color(0xFF9b11ba)
+val orange = Color(0xFFdb660d)
+val redOrange = Color(0xFFe84a23)
+val green = Color(0xFF0ddb25)
+val Blue = Color(0xFF140ddb)
+val brightBlue = Color(0xFF027cf5)
+val darkGray = Color(0xFF1a1717)
+val darkBlue = Color(0xFF18192b)
+val white = Color(0xFFF3F3F3)
+val gray = Color(0xFF3F3F3F)
+val blueGray = Color(0xFF404352)
+val nightDark = Color(0xFF403757)
